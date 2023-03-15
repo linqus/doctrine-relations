6a65360e396db0fc61fdd7c7331837522d7888ab
Karol Linkiewicz <karol@linkiewicz.pl>
2023-03-15 22:24:38 +0100

    voting dirty way

--- src/Controller/AnswerController.php
+++ src/Controller/AnswerController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Answer;
+use App\Repository\AnswerRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,22 +16,25 @@ class AnswerController extends AbstractController
     /**
      * @Route("/answers/{id}/vote", methods="POST", name="answer_vote")
      */
-    public function answerVote($id, LoggerInterface $logger, Request $request)
+    public function answerVote($id, LoggerInterface $logger, Request $request, EntityManagerInterface $entityManager)
     {
         $data = json_decode($request->getContent(), true);
         $direction = $data['direction'] ?? 'up';
 
         // todo - use id to query the database
-
+        $answer = $entityManager->getRepository(Answer::class)->find($id);
+        $currentVoteCount = $answer->getVotes();
         // use real logic here to save this to the database
         if ($direction === 'up') {
             $logger->info('Voting up!');
-            $currentVoteCount = rand(7, 100);
+            $currentVoteCount = $currentVoteCount + 1;
+
         } else {
             $logger->info('Voting down!');
-            $currentVoteCount = rand(0, 5);
+            $currentVoteCount = $currentVoteCount - 1;
         }
-
+        $answer->setVotes($currentVoteCount);
+        $entityManager->flush();
         return $this->json(['votes' => $currentVoteCount]);
     }
 }
