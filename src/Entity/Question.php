2b9aeb05e985d1a59a622b1ff361173564c65142
Karol Linkiewicz <karol@linkiewicz.pl>
2023-03-16 23:16:34 +0100

    naprawa use repository

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Repository\QuestionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
