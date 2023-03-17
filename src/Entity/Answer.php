18367b02210d565e47a80c1b63e2ebfef2bd1ae3
Karol Linkiewicz <karol@linkiewicz.pl>
2023-03-17 19:44:30 +0100

    helper method returning string repr of question

--- src/Entity/Answer.php
+++ src/Entity/Answer.php
@@ -106,6 +106,14 @@ class Answer
         return $this->question;
     }
 
+    public function getQuestionText(): string
+    {
+        if (!$this->getQuestion()) {
+            return '';
+        }
+        return (string) $this->getQuestion()->getQuestion();
+    }
+
     public function setQuestion(?Question $question): self
     {
         $this->question = $question;
